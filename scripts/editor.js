d29591d8916a3a4255fe1409e7e397eeeaea340e
snada <stefano.nada@gmail.com>
2016-02-08 02:21:41 +0100

    [11] Editor better draft

--- scripts/editor.js
+++ scripts/editor.js
@@ -2,11 +2,13 @@ $('#editor-dragbar').on('mousedown', function(e) {
   e.preventDefault();
 
   $(document).on('mousemove', function(e) {
-    $('#position').html(e.pageX +', '+ e.pageY);
-    $('#editor-sidebar').css("width",e.pageX+2);
-    $('#editor-preview').css("left",e.pageX+2);
-  })
-  console.log("leaving mouseDown");
+    if(e.pageX < $(document).width() / 2 && e.pageX >= 0) {
+      $('#editor-sidebar').css('width', e.pageX);
+      $('#editor-dragbar').css('left', e.pageX);
+      $('#editor-preview').css('margin-left', e.pageX);
+      $('#editor-sidebar-commands').css('width', e.pageX);
+    }
+  });
 });
 
 $(document).on('mouseup', function(e){
